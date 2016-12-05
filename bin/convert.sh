1db6cc41089ba8f4edb403e9751a0a6f250493b1
Andriy Kulynyak <kulynyak@gmail.com>
2016-12-05 13:34:39 +0200

    minor config changes

---
+++ bin/convert.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+MOVIES=~/Movies
+#MOVIES="/Volumes/MP 0830/Meetings/2016"
+find "$MOVIES" -name '*.mov' -exec sh -c 'ffmpeg -i "$0" -vcodec libx264 -crf 23 "${0%%.mov}.mp4"' {} \;
+exit;
