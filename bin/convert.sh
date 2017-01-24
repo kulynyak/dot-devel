3e880d04a5327fd7522bd2a699aa7d135599da9f
Andriy Kulynyak <kulynyak@gmail.com>
2017-01-24 11:36:51 +0200

    Start using meld and sublime for git related task

--- bin/convert.sh
+++ bin/convert.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/sh
 MOVIES=~/Movies
 #MOVIES="/Volumes/MP 0830/Meetings/2016"
 find "$MOVIES" -name '*.mov' -exec sh -c 'ffmpeg -i "$0" -vcodec libx264 -crf 23 "${0%%.mov}.mp4"' {} \;
